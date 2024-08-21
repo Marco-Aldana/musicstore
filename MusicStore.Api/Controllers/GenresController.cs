ed2312384c85ad171ff0dff82f1a4a5d2345852b
marco <marco@MAAD>
2024-08-21 01:36:36 -0600

    Agregar archivos de proyecto.

---
+++ MusicStore.Api/Controllers/GenresController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicStore.Entities;
+using MusicStore.Repositories;
+
+namespace MusicStore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    public class GenresController : ControllerBase
+    {
+        private readonly GenreRepository repository;
+
+        public GenresController(GenreRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Genre>> Get()
+        {
+            var data = repository.Get();
+            return Ok(data);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Genre> Get(int id) { 
+            var item = repository.Get(id); 
+            return item is not null ? Ok(item) : NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<Genre> Post(Genre item)
+        {
+            repository.Add(item);
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Genre item)
+        {
+            repository.Update(id, item);
+            return Ok(item);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ MusicStore.Repositories/GenreRepository.cs
@@ -0,0 +1,53 @@
+﻿using MusicStore.Entities;
+
+namespace MusicStore.Repositories
+{
+    public class GenreRepository
+    {
+        private readonly List<Genre> genresList;
+        public GenreRepository()
+        {
+            genresList =
+            [
+                new Genre { Id=1, Name="Cumbia" },
+                new Genre { Id=2, Name="Reggae" },
+                new Genre { Id=30, Name="Balada" },
+            ];
+        }
+
+        public List<Genre> Get()
+        {
+            return genresList;
+        }
+
+        public Genre? Get(int id)
+        {
+            return genresList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Genre Add(Genre genre)
+        {
+            var lastItem = genresList.MaxBy(x => x.Id);
+            genre.Id = lastItem is null ? 1 : lastItem.Id + 1;
+            genresList.Add(genre);
+            return genre;
+        }
+
+        public void Update(int id, Genre genre)
+        {
+            var item = Get(id);
+            if (item is not null)
+            {
+                item.Name = genre.Name;
+                item.Status = genre.Status;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var item = Get(id);
+            if (item is not null)
+                genresList.Remove(item);
+        }
+    }
+}
